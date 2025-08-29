3aa2edf923d5d23c6b561d87a6bd357febc71fe4
null186 <null186@163.com>
2025-08-30 00:49:34 +0800

    modify config

--- demo/main.cpp
+++ demo/main.cpp
@@ -10,32 +10,33 @@
 
 using namespace autumn;
 int main() {
-    autumn::LogConfig log_config("./", 16, 1024, LogType::DEFAULT, LogPriority::DEFAULT);
-    logger_t logger = 0;
-    create_logger(&logger, log_config);
+  autumn::LogConfig log_config{"./", 16, 1024, LogType::DEFAULT,
+                               LogPriority::DEFAULT};
+  logger_t logger = 0;
+  create_logger(&logger, log_config);
 
-    std::vector<std::thread> threads;
-    for (int i = 0; i < 20; ++i) {
-        threads.emplace_back([=]() {
-            log_print(logger, LogType::MAIN, LogPriority::VERBOSE, "Network", __FILE_NAME__,
-                      __LINE__, "%s", "Hello Main!");
-            log_print(logger, LogType::RADIO, LogPriority::DEBUG, "Sim", __FILE_NAME__, __LINE__,
-                      "%s", "Hello Radio!");
-            log_print(logger, LogType::SYSTEM, LogPriority::INFO, "Service", __FILE_NAME__,
-                      __LINE__, "%s", "Hello System!");
-            log_print(logger, LogType::SECURITY, LogPriority::WARN, "Hook", __FILE_NAME__, __LINE__,
-                      "%s", "Hello Security!");
-            log_print(logger, LogType::KERNEL, LogPriority::ERROR, "Thread", __FILE_NAME__,
-                      __LINE__, "%s", "Hello Kernel!");
-        });
-    }
+  std::vector<std::thread> threads;
+  for (int i = 0; i < 20; ++i) {
+    threads.emplace_back([=]() {
+      log_print(logger, LogType::MAIN, LogPriority::VERBOSE, "Network",
+                __FILE_NAME__, __LINE__, "%s", "Hello Main!");
+      log_print(logger, LogType::RADIO, LogPriority::DEBUG, "Sim",
+                __FILE_NAME__, __LINE__, "%s", "Hello Radio!");
+      log_print(logger, LogType::SYSTEM, LogPriority::INFO, "Service",
+                __FILE_NAME__, __LINE__, "%s", "Hello System!");
+      log_print(logger, LogType::SECURITY, LogPriority::WARN, "Hook",
+                __FILE_NAME__, __LINE__, "%s", "Hello Security!");
+      log_print(logger, LogType::KERNEL, LogPriority::ERROR, "Thread",
+                __FILE_NAME__, __LINE__, "%s", "Hello Kernel!");
+    });
+  }
 
-    log_print(logger, LogType::KERNEL, LogPriority::ERROR, "", "", 0, nullptr);
+  log_print(logger, LogType::KERNEL, LogPriority::ERROR, "", "", 0, nullptr);
 
-    for (auto& thread : threads) {
-        thread.join();
-    }
+  for (auto& thread : threads) {
+    thread.join();
+  }
 
-    destroy_logger(logger);
-    return 0;
+  destroy_logger(logger);
+  return 0;
 }
--- include/autumn/config.h
+++ include/autumn/config.h
@@ -11,46 +11,20 @@
 
 namespace autumn {
 
-enum class AutumnId : uint32_t { NORMAL, EVENT, STATISTICS, DUMP };
-
 /**
  * Autumn log config.
  */
 struct LogConfig {
   /** Work dir. */
-  const std::string work_dir;
+  std::string work_dir;
   /** Maximum number of files in a log type directory. */
-  const size_t max_files;
+  size_t max_files;
   /** Maximum size of a log file. */
-  const size_t max_file_size; /* KiB */
+  size_t max_file_size; /* KiB */
   /** {@link LogPriority} values. */
-  const LogType type_mask;
+  LogType type_mask;
   /** {@link LogPriority} values. */
-  const LogPriority priority;
-
-  LogConfig(std::string work_dir, const size_t max_files,
-            const size_t max_file_size, const LogType type_mask,
-            const LogPriority priority)
-      : work_dir(std::move(work_dir)),
-        max_files(max_files),
-        max_file_size(max_file_size),
-        type_mask(type_mask),
-        priority(priority) {}
+  LogPriority priority;
 };
-
-/**
- * Autumn event config.
- */
-struct EventConfig {};
-
-/**
- * Autumn statistics config.
- */
-struct StatisticsConfig {};
-
-/**
- * Autumn dump config.
- */
-struct DumpConfig {};
 
 }  // namespace autumn
