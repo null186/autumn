15264c6e5e1894d97497db71bc36ed0c79ba954c
null186 <null186@163.com>
2025-08-30 16:03:03 +0800

    modify writer

---
+++ src/writer/console_writer.h
@@ -0,0 +1,10 @@
+//
+// Created by J Chen on 2025/8/30.
+//
+
+#ifndef AUTUMNLOG_SRC_WRITER_CONSOLE_WRITER_H_
+#define AUTUMNLOG_SRC_WRITER_CONSOLE_WRITER_H_
+
+class ConsoleWriter {};
+
+#endif  // AUTUMNLOG_SRC_WRITER_CONSOLE_WRITER_H_
---
+++ src/writer/hook_writer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by J Chen on 2025/8/30.
+//
+
+#include "hook_writer.h"
