0f1aa2f6535a86258ca52f408ef3cc7e52812a04
null186 <null186@163.com>
2025-09-16 09:39:32 +0800

    add mmap & use filesystem

--- src/writer/file.cpp
+++ src/writer/file.cpp
@@ -5,19 +5,110 @@
 #include "src/writer/file.h"
 
 #include <dirent.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #include <cassert>
 #include <regex>
+#include <sstream>
+
+#include "src/utils/inner_log.h"
 
 namespace autumn {
 
-static const std::string kPrefix = "autumn-";
-static const std::string kExtension = ".log";
+static constexpr char kPrefix[] = "autumn-";
+static constexpr char kExtension[] = ".log";
 
-void LogFile::Open() { fp_ = fopen(path_.c_str(), "ab+"); }
+bool MmapFile::Open() {
+  static constexpr int kMode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP;
+  fd_ = open(path().c_str(), O_RDWR | O_CREAT, kMode);
+  if (fd_ == -1) {
+    return false;
+  }
 
-void LogFile::Close() {
+  struct stat st;
+  if (fstat(fd_, &st) == -1) {
+    Reset();
+    return false;
+  }
+  assert(st.st_size >= 0 && st.st_size <= static_cast<off_t>(max_size()));
+  size_ = st.st_size;
+
+  if (ftruncate(fd_, static_cast<off_t>(max_size())) == -1) {
+    Reset();
+    return false;
+  }
+
+  static constexpr int kProt = PROT_READ | PROT_WRITE;
+  void* data = mmap(nullptr, max_size(), kProt, MAP_SHARED, fd_, 0);
+  if (!data) {
+    Reset();
+    return false;
+  }
+
+  data_ = static_cast<char*>(data);
+  return true;
+}
+
+void MmapFile::Close() { Reset(); }
+
+void MmapFile::Remove() {
+  if (data_ != nullptr) {
+    munmap(data_, max_size());
+    data_ = nullptr;
+  }
+
+  if (fd_ != -1) {
+    close(fd_);
+    fd_ = -1;
+  }
+
+  std::remove(path().c_str());
+}
+
+bool MmapFile::CanWrite(size_t size) const {
+  return size_ + size <= max_size();
+}
+size_t MmapFile::Write(const std::string& data) {
+  if (!data_) {
+    return 0;
+  }
+  std::copy(data.begin(), data.end(), data_ + size_);
+  size_ += data.size();
+  return data.size();
+}
+
+size_t MmapFile::Write(const std::vector<char>& data) {
+  if (!data_) {
+    return 0;
+  }
+  std::copy(data.begin(), data.end(), data_ + size_);
+  size_ += data.size();
+  return data.size();
+}
+
+void MmapFile::Reset() {
+  if (data_ != nullptr) {
+    msync(data_, size_, MS_SYNC);
+    munmap(data_, max_size());
+    data_ = nullptr;
+  }
+
+  if (fd_ != -1) {
+    ftruncate(fd_, static_cast<off_t>(size_));
+    close(fd_);
+    fd_ = -1;
+  }
+}
+
+bool Normalile::Open() {
+  fp_ = fopen(path().c_str(), "ab+");
+  return fp_ != nullptr;
+}
+
+void Normalile::Close() {
   if (!fp_) {
     return;
   }
@@ -26,18 +117,14 @@ void LogFile::Close() {
   fp_ = nullptr;
 }
 
-void LogFile::Remove() { std::remove(path_.c_str()); }
+void Normalile::Remove() { std::remove(path().c_str()); }
 
-std::time_t LogFile::ModifyTime() const { return mtime_; }
-
-size_t LogFile::Index() const { return index_; }
-
-bool LogFile::CanWrite(size_t size) const {
-  return size_ + size <= max_file_size_;
+bool Normalile::CanWrite(size_t size) const {
+  return size_ + size <= max_size();
 }
 
-size_t LogFile::Write(const std::string& data) {
-  size_t written = fwrite(data.c_str(), sizeof(char), data.size(), fp_);
+size_t Normalile::Write(const std::string& data) {
+  const size_t written = fwrite(data.c_str(), sizeof(char), data.size(), fp_);
   size_ += written;
   if (written == data.size()) {
     fflush(fp_);
@@ -45,47 +132,74 @@ size_t LogFile::Write(const std::string& data) {
   return written;
 }
 
-LogDir::LogDir(size_t max_files, size_t max_file_size, std::string dir_path)
+size_t Normalile::Write(const std::vector<char>& data) {
+  const size_t written = fwrite(data.data(), sizeof(char), data.size(), fp_);
+  size_ += written;
+  if (written == data.size()) {
+    fflush(fp_);
+  }
+  return written;
+}
+
+LogDir::LogDir(size_t max_files, size_t max_file_size, fs::path dir_path)
     : max_files_(max_files),
       max_file_size_(max_file_size),
-      dir_path_(std::move(dir_path)) {
-  Init();
-}
+      dir_path_(std::move(dir_path)) {}
 
 LogDir::~LogDir() {
   if (!list_.empty()) {
     list_.back()->Close();
   }
-  list_.clear();
 }
 
 void LogDir::Init() {
-  DIR* dir = opendir(dir_path_.c_str());
-  if (dir == nullptr) {
+  std::error_code ec;
+  auto dir_status = fs::status(dir_path_, ec);
+  if (ec) {
+    ilog << "error getting " << dir_path_ << " status: " << ec.message()
+         << end_line;
     return;
   }
 
-  dirent* d;
-  struct stat s{};
+  if (fs::is_directory(dir_status)) {
+    ilog << dir_path_ << "is not a dir" << end_line;
+  }
+
   std::string content;
   content.append(kPrefix).append("(\\d+)").append(kExtension);
   const std::regex regex(content);
-  while ((d = readdir(dir)) != nullptr) {
-    std::string filename = d->d_name;
+  for (const auto& entry : fs::directory_iterator(dir_path_, ec)) {
+    if (ec) {
+      ilog << "error getting " << dir_path_ << " entry: " << ec.message()
+           << end_line;
+      break;
+    }
+
+    const fs::path& path = entry.path();
+    auto file_status = fs::status(path, ec);
+    if (ec) {
+      ilog << "error getting " << path << " status: " << ec.message()
+           << end_line;
+      continue;
+    }
+    if (!entry.is_regular_file()) {
+      ilog << path << " is not a regular file: " << end_line;
+      continue;
+    }
+
+    const std::string& filename = path.filename().string();
     std::smatch match;
     if (!std::regex_match(filename, match, regex)) {
       continue;
     }
-    std::string index = match.str(1);
-    if (index.empty()) {
+    if (match.str(1).empty()) {
       continue;
     }
-    std::string path = dir_path_ + "/" + filename;
-    if (stat(path.c_str(), &s) != 0) {
-      continue;
-    }
-    auto new_file = std::make_unique<LogFile>(max_file_size_, std::stoi(index),
-                                              path, s.st_mtime, s.st_size);
+    const auto index = static_cast<size_t>(std::stoi(match.str(1)));
+    const auto mtime = fs::last_write_time(path);
+    auto size = fs::file_size(path);
+    LogFile::Meta meta{index, max_file_size_, mtime, path};
+    auto new_file = std::make_unique<Normalile>(meta, size);
     list_.push_back(std::move(new_file));
   }
 
@@ -96,8 +210,7 @@ void LogDir::Init() {
 
   list_.sort(
       [](const std::unique_ptr<LogFile>& a, const std::unique_ptr<LogFile>& b) {
-        assert(a && b);
-        return a->ModifyTime() < b->ModifyTime();
+        return a->mtime() < b->mtime();
       });
   list_.back()->Open();
 }
@@ -112,14 +225,26 @@ LogFile* LogDir::GetLogFile(size_t size) {
   return list_.back().get();
 }
 
+fs::path LogDir::GenPath(const size_t index) const {
+  if (!fs::exists(dir_path_)) {
+    fs::create_directories(dir_path_);
+  }
+  std::stringstream ss;
+  ss << std::setfill('0') << std::setw(log10(max_files_) + 1) << (index);
+  std::string filename;
+  filename.append(kPrefix).append(ss.str()).append(kExtension);
+  return dir_path_ / filename;
+}
+
 void LogDir::RollFile() {
   size_t index = 0;
   if (!list_.empty()) {
-    index = (list_.back()->Index() + 1) % max_files_;
+    index = (list_.back()->index() + 1) % max_files_;
     list_.back()->Close();
   }
-  std::string path = kPrefix + std::to_string(index) + kExtension;
-  auto new_file = std::make_unique<LogFile>(max_file_size_, index, path);
+
+  LogFile::Meta meta{index, max_file_size_, {}, GenPath(index)};
+  auto new_file = std::make_unique<Normalile>(meta, 0);
   list_.push_back(std::move(new_file));
 
   if (list_.size() > max_files_) {
@@ -127,7 +252,6 @@ void LogDir::RollFile() {
     for (size_t i = 0; i < oversize; ++i) {
       auto* oldest_file = list_.front().get();
       oldest_file->Remove();
-      delete oldest_file;
       list_.pop_front();
     }
   }
@@ -137,7 +261,9 @@ void LogDir::RollFile() {
 FileWriter::FileWriter(size_t max_files, size_t max_file_size,
                        std::string dir_path)
     : log_dir_(std::make_unique<LogDir>(max_files, max_file_size,
-                                        std::move(dir_path))) {}
+                                        std::move(dir_path))) {
+  log_dir_->Init();
+}
 
 uint64_t FileWriter::Write(const std::string& data) {
   std::lock_guard lock_guard(mutex_);
--- src/writer/file.h
+++ src/writer/file.h
@@ -5,56 +5,96 @@
 #ifndef SRC_WRITER_FILE_H_
 #define SRC_WRITER_FILE_H_
 
+#include <filesystem>
+
 #include <list>
 #include <mutex>
 
 namespace autumn {
 
+namespace fs = std::filesystem;
+
 class LogFile {
  public:
-  LogFile(size_t max_file_size, size_t index, std::string path,
-          std::time_t mtime, size_t size)
-      : max_file_size_(max_file_size),
-        index_(index),
-        path_(std::move(path)),
-        mtime_(mtime),
-        size_(size) {}
-  LogFile(size_t max_file_size, size_t index, std::string path)
-      : max_file_size_(max_file_size), index_(index), path_(std::move(path)) {}
-  ~LogFile() = default;
+  struct Meta {
+    const size_t index_;
+    const size_t max_size_;
+    const fs::file_time_type mtime_;
+    const fs::path path_;
+  };
 
-  void Open();
-  void Close();
-  void Remove();
-  std::time_t ModifyTime() const;
-  size_t Index() const;
-  bool CanWrite(size_t size) const;
-  size_t Write(const std::string& data);
+  LogFile(const Meta& info) : meta_(info) {}
+  virtual ~LogFile() = default;
+
+  virtual bool Open() = 0;
+  virtual void Close() = 0;
+  virtual void Remove() = 0;
+  virtual bool CanWrite(size_t size) const = 0;
+  virtual size_t Write(const std::string& data) = 0;
+  virtual size_t Write(const std::vector<char>& data) = 0;
+
+  size_t index() const { return meta_.index_; }
+  size_t max_size() const { return meta_.max_size_; }
+  const fs::file_time_type& mtime() const { return meta_.mtime_; }
+  const fs::path& path() const { return meta_.path_; }
 
  private:
-  const size_t max_file_size_;
-  const size_t index_ = 0;
-  std::string path_;
-  std::time_t mtime_ = 0;
+  const Meta meta_;
+};
+
+class MmapFile final : public LogFile {
+ public:
+  MmapFile(const Meta& meta, size_t size) : LogFile(meta), size_(size) {}
+  ~MmapFile() override = default;
+
+  bool Open() override;
+  void Close() override;
+  void Remove() override;
+  bool CanWrite(size_t size) const override;
+  size_t Write(const std::string& data) override;
+  size_t Write(const std::vector<char>& data) override;
+
+ private:
+  int fd_ = -1;
+  char* data_ = nullptr;
+  size_t size_ = 0;
+
+  void Reset();
+};
+
+class Normalile final : public LogFile {
+ public:
+  Normalile(const Meta& meta, size_t size) : LogFile(meta), size_(size) {}
+  ~Normalile() override = default;
+
+  bool Open() override;
+  void Close() override;
+  void Remove() override;
+  bool CanWrite(size_t size) const override;
+  size_t Write(const std::string& data) override;
+  size_t Write(const std::vector<char>& data) override;
+
+ private:
   FILE* fp_ = nullptr;
   size_t size_ = 0;
 };
 
 class LogDir {
  public:
-  LogDir(size_t max_files, size_t max_file_size, std::string dir_path);
+  LogDir(size_t max_files, size_t max_file_size, fs::path dir_path);
   ~LogDir();
 
+  void Init();
   LogFile* GetLogFile(size_t size);
 
  private:
-  void Init();
-  void RollFile();
-
   const size_t max_files_;
   const size_t max_file_size_;
-  const std::string dir_path_;
+  const fs::path dir_path_;
   std::list<std::unique_ptr<LogFile>> list_;
+
+  void RollFile();
+  fs::path GenPath(size_t index) const;
 };
 
 class FileWriter {
